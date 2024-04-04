dc983dbba1d6b2c383e8490b72185c62ac838117
Wiar8 <sebastian.noguera30@gmail.com>
2024-04-04 19:09:29 -0400

    Fix bug (?)

--- utils/tts.js
+++ utils/tts.js
@@ -1,10 +1,17 @@
 export const tts = (text) => {
-  // Detiene cualquier reproducción en curso antes de iniciar una nueva
-  speechSynthesis.cancel();
+  if ("speechSynthesis" in window) {
+    // Detiene cualquier reproducción en curso antes de iniciar una nueva
+    window.speechSynthesis.cancel();
 
-  const voice = new SpeechSynthesisUtterance();
-  voice.text = text;
-  voice.lang = "es-ES";
-  voice.onend = () => window.location.reload(); // Refresca la página al terminar de hablar
-  speechSynthesis.speak(voice);
+    const voice = new SpeechSynthesisUtterance();
+    voice.text = text;
+    voice.lang = "es-ES";
+    // Elimina el recargo de página al terminar de hablar
+    // voice.onend = () => window.location.reload();
+    voice.onend = () => window.location.reload(); // Refresca la página al terminar de hablar
+
+    window.speechSynthesis.speak(voice);
+  } else {
+    console.error("La síntesis de voz no está disponible en este navegador.");
+  }
 };
